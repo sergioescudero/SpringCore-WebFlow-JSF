e84aa43e7979fdf5ff35fa0d0a96786c07e864d6
Sergio Escudero <sergio.escudero@me.com>
2013-04-11 18:11:26 +0200

    beans

---
+++ 06_Seguros/src/main/java/com/seguros/beans/Persona.java
@@ -0,0 +1,76 @@
+package com.seguros.beans;
+
+import java.util.List;
+
+public class Persona {
+
+	private Integer id;
+	private String nombre;
+	private String apellidos;
+	private String dni;
+	private String direccion;
+	private int antiguedad;
+	private int edad;
+	private String codigoPostal;
+	private String poblacion;
+	List <TipoCarnet> carnet;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public int getAntiguedad() {
+		return antiguedad;
+	}
+	public void setAntiguedad(int antiguedad) {
+		this.antiguedad = antiguedad;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+	public String getPoblacion() {
+		return poblacion;
+	}
+	public void setPoblacion(String poblacion) {
+		this.poblacion = poblacion;
+	}
+	
+	
+	
+	
+}
